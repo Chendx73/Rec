2354ffc5fdaf858b5b923f5e0fb703595b5059f7
Chenyu <chendx66@163.com>
2024-05-06 19:39:15 +0800

    First commit

--- Preprocessing/Netflix Prize.py
+++ Preprocessing/Netflix Prize.py
@@ -21,11 +21,25 @@ def process_movie_title(fpath: str) -> dict:
 
 
 def transform2triple(fpath: str):
+    """
+    将训练集中所有数据->用户id,电影id,评分
+    :param fpath:
+    :return:
+    """
     all_files = os.listdir(fpath + "/datasets/netflix_prize/mini_training_set")
+    fw = open(fpath + '/datasets/netflix_prize/output/data.txt', 'w')
+    flag = True
+    movie_id = None
     for path in all_files:
-        with open(fpath + "/datasets/netflix_prize/mini_training_set/" + path,'r') as f:
-            print(f.readline())
-            exit(0)
+        with open(fpath + "/datasets/netflix_prize/mini_training_set/" + path, 'r') as f:
+            for i in f:
+                if flag:
+                    movie_id = i.split(':\n')[0]
+                    flag = False
+                else:
+                    user_id, score = i.strip(',')[0], i.strip(',')[1]
+                    fw.write(user_id + "," + movie_id + "," + score + "\n")
+    fw.close()
+
 
 transform2triple(f_path)
-
